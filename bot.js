4140b0f132977a2afcdde529cbf520b810019021
PivasMaster <alyoscha.chernigovsky@yandex.ru>
2024-12-20 04:13:34 +0800

    tg bot upd

--- bot.js
+++ bot.js
@@ -1,8 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api');
 
+
 const token = '7955694028:AAEDg782HLJJGv-PHhVMg0zdknnC2YvNMGA';
 const bot = new TelegramBot(token, {polling: true});
 
+
+const yourName = 'Черниговским Алекскеем';
+const octagonSite = 'https://students.forus.ru/'; 
+
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Привет, октагон!");
+});
+
+bot.onText(/\/help/, (msg) => {
+  const helpText = `Список команд:\n\n` +
+    `/help -  Показать этот список команд\n` +
+    `/site -  Ссылка на сайт Октагон\n` +
+    `/creator -  Информация о создателе бота`;
+  bot.sendMessage(msg.chat.id, helpText);
+});
+
+bot.onText(/\/site/, (msg) => {
+  bot.sendMessage(msg.chat.id, octagonSite);
+});
+
+bot.onText(/\/creator/, (msg) => {
+  bot.sendMessage(msg.chat.id, `Бот создан: ${yourName}`);
 });
